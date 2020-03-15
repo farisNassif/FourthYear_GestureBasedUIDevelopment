5be729c9d6d2550165640bc63117f031887bb4ec
Alex Cherry <G00347106@gmit.ie>
2020-03-15 17:23:20 +0000

    updated menus and added game select menu

---
+++ Assets/Scripts/MenuScripts/GameSelectScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Script for loading into game one
+
+public class GameSelectScript : MonoBehaviour
+{
+    
+    public void SelectGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
