009f233d9ebbe4c02372cadf9896ba2a766a7048
Alex Cherry <G00347106@gmit.ie>
2020-03-15 12:57:10 +0000

    added simple menu with functional button

---
+++ Assets/Scripts/MenuScripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// This Script is for managing the main menu upon loading the application
+// References : https://www.youtube.com/watch?v=zc8ac_qUXQY&t=583s (Brackeys tutorial on scene management)
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Select Game function 
+    public void SelectGame()
+    {
+        // Loads main scene (will be changed when updating menus)
+        SceneManager.LoadScene("MainScene");
+    }
+}
