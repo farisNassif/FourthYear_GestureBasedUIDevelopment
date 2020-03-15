30e79d7a3a92725ff223d76b09f15854294c0126
farisNassif <g00347032@gmit.ie>
2020-03-15 17:36:22 +0000

    Timer mechanic fully implemented, audio added for countdown

--- Assets/Scripts/Game/TimeManager.cs
+++ Assets/Scripts/Game/TimeManager.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 /* Class that manages the timer for the Baloon bursting game */
 public class TimeManager : MonoBehaviour
 {
     float currentTime = 0f;
-    float startingTime = 10f;
-
+    float startingTime = 30f;
+    bool playedSound = false;
     public Text countdownTimer; 
      
     /* When the game begins, set the Current time to 30 */
@@ -33,6 +34,16 @@ public class TimeManager : MonoBehaviour
         if (currentTime < 10)
         {
             countdownTimer.color = Color.red;
+        }
+
+        /* Start a countdown when 7 seconds remain */
+        if (currentTime.ToString("0") == "7")
+        {
+            if(playedSound == false)
+            {
+                SoundManagerScript.CountdownClip();
+                playedSound = true; // To make sure this doesn't execute multiple times to not deafen the user
+            }    
         }
 
         /* Times up! Game over */
---
+++ Assets/Scripts/Sounds/SoundManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    public static AudioClip CountdownSound;
+    static AudioSource AudioSrc;
+    private float soundVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CountdownSound = Resources.Load<AudioClip>("Countdown");
+
+        AudioSrc = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSrc.volume = soundVolume;
+    }
+
+    public static void CountdownClip()
+    {
+        AudioSrc.PlayOneShot(CountdownSound);
+    }
+
+}
