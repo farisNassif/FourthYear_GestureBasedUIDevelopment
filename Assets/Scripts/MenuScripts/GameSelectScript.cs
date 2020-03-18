cc231de811d55858918591c95213e5523bf35edc
Alex Cherry <G00347106@gmit.ie>
2020-03-18 13:47:52 +0000

    added game over sequence, music not replaying yet on retry

--- Assets/Scripts/MenuScripts/GameSelectScript.cs
+++ Assets/Scripts/MenuScripts/GameSelectScript.cs
@@ -8,9 +8,29 @@ using Windows.Kinect;
 
 public class GameSelectScript : MonoBehaviour
 {
+    public TimeManager soundManager;
     
+    // select game 
     public void SelectGame()
     {
         SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1f;
+    }
+
+    // return to menu scene (no button)
+    public void Return()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    // restart game and return music to true (yes button)
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1f;
+
+        // reset music 
+        TimeManager.playedSound = true;
+        TimeManager.playedMusic = true;
     }
 }
