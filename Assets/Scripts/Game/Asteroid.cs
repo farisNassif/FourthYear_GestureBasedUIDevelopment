ca09dce45cd5553462f6b3fb848734c06a0a5c42
farisNassif <g00347032@gmit.ie>
2020-03-21 23:04:45 +0000

    Projectiles now randomly spawning at good locations and can collide with the player

--- Assets/Scripts/Game/Asteroid.cs
+++ Assets/Scripts/Game/Asteroid.cs
@@ -17,7 +17,7 @@ public class Asteroid : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if(transform.position.x < screenBounds.x * 2){
+        if(transform.position.x < screenBounds.x){
             Destroy(this.gameObject);
         }
     }
