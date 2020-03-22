8210667ebfcc664a00ee82e4c507d44398586a43
farisNassif <g00347032@gmit.ie>
2020-03-22 15:04:45 +0000

    Added shoot charge/recharge and charge display

--- Assets/Scripts/Game/ShootScript.cs
+++ Assets/Scripts/Game/ShootScript.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/* Class that manages the bird shooting */
 public class ShootScript : MonoBehaviour
 {
     public Transform bullet;
     public GameObject bulletPrefab;
-
+    
+    /* Player starts with full charge */
+    public int currentCharge = 100;
+    /* Should be displaying the power bar or percentage, 100% should be required to shoot */
+    public Text shootCharge;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        /* Max the charge out at 100 */
+        if (currentCharge != 100)
+        {
+            currentCharge++;
+        }
+
+        /* Display charge in real time */
+        shootCharge.text = "Shoot charge: " + currentCharge + "%";
+
+        if (Input.GetKeyDown(KeyCode.Space) && currentCharge == 100)
         {
             // shoot function for keyboard
             Shoot();
+            currentCharge = 0;
             Debug.Log("Shoot");
         }
     }
