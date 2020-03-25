e130b4c63eb818dbe657bff62131511cbf7a5a2a
farisNassif <g00347032@gmit.ie>
2020-03-25 19:23:32 +0000

    Balloon game high scores stored and can be loaded on different startups

--- Assets/Scripts/BaloonGameScripts/SceneManagers/ScoreManager.cs
+++ Assets/Scripts/BaloonGameScripts/SceneManagers/ScoreManager.cs
@@ -14,28 +14,34 @@ public class ScoreManager : MonoBehaviour
     private int rareBaloon = 10;
     /* Text display for in game purposes */
     public Text scoreText;
-    
+    public Text highScoreText;
+    /* If the current player set a new high score */
+    public bool setNewHighScore = false;
+
     /* Singleton Instance */
     private void Awake()
     {
         instance = this;
     }
 
+    void Start() {
+        /* Get the previously stored high score and show it */
+        GetHighScore();
+    }
+
     /* Increments current players score */
     public void UpdateScore(int CollisionType)
     {
-        UpdateHighScore(); // Check if current score is better than previous high (NOT IMPLEMENTED YET)
-
         /* If the player collides with a green balooon, execute this */
         if(CollisionType == 0) 
         {
-            // Increment Score by 1
+            /* Increment Score by 1 */
             score = score + goodBaloon;
         } 
         /* If the player collides with a bad balooon, execute this */
         else if (CollisionType == 1)
         {
-            // Decrement Score by 3
+            /* Decrement Score by 3 */
             score -= badBaloon; 
 
             /* Ensuring the score won't ever go below 0 */
@@ -47,13 +53,16 @@ public class ScoreManager : MonoBehaviour
         /* If the player collides with a rare balooon, execute this */
         else 
         {
-            // Increment Score by 5
+            /* Increment Score by 5 */
             score += rareBaloon; 
         }
 
+        /* Check if current score is better than previous high */
+        UpdateHighScore(); 
+
+        /* Set the text fields to reflect scores */
+        highScoreText.text = "High Score: [" + highScore.ToString() + "]";
         scoreText.text = "Score: [" + score.ToString() + "]";
-        
-        // gameOverScoreText.text = score.ToString();
     }
 
     /* Basically if current score is greater than current high score, score = new high score */
@@ -61,9 +70,27 @@ public class ScoreManager : MonoBehaviour
     {
         if (score > highScore)
         {
-            highScore = score;
-           // highScoreText.text = highScore.ToString();
+            /* New high score set */
+            highScore = score + 1;
+            /* Display to the user the new high score */
+            highScoreText.text = "High Score: [" + highScore.ToString() + "]";
+            /* Save score locally so it persists */
+            SetHighScore();
+            /* Player set a new High Score! */
+            setNewHighScore = true;
         }
+    }
+
+    /* Set score locally so it persists */
+    public void SetHighScore()
+    {
+        PlayerPrefs.SetInt("balloonHS", score); 
+    }
+
+    /* On start set the high score to what was saved locally */
+    public void GetHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("balloonHS");
     }
 
     /* So that local score doesn't persist after win or lose condition */
